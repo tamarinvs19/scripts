6e1043a80d55a115d1d61ed6034e5ef7882a24b1
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2020-08-18 08:59:15 +0300

    upd load_dotfiles

--- load_dotfiles.sh
+++ load_dotfiles.sh
@@ -12,6 +12,8 @@ cp ~/.config/xkbcomp/stru xkbcomp/stru || echo "Can not copy xkbcomp config"
 cp ~/.zshrc zsh/zshrc || echo "Can not copy zsh config"
 cp ~/.oh-my-zsh/custom/themes/green.zsh-theme zsh/green.zsh-theme || echo "Can not copy oh-my-zsh config"
 cp ~/.config/rofi/config rofi/config || echo "Can not copy rofi config"
+cp ~/.config/polybar/config polybar/config || echo "Can not copy polybar config"
+cp ~/.config/inkscape-shortcut-manager/* inkscape-shortcut-manager || echo "Can not copy inkscape config"
 
 git pull
 git add .
