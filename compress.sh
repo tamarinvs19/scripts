9bb9ad5e2e6ce97a41ef1f2a25c13a91952baf4d
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2020-09-23 16:43:41 +0300

    add compress and slice_video

---
+++ compress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -eq 2 ]
+then
+    output_file=$1
+    list=$2
+    $(ffmpeg -f concat -i $files -c:v libx264 -ac 2 -c:a aac -strict -2 -b:a 128k -crf 28 -r 24 $output_file)
+else
+    echo "Usage: ./compress.sh <OUTPUT_FILE> <LIST FILE>"
+    echo Format of LIST_FILE:
+    echo "   file 'NAME'"
+    echo "   file 'NAME'"
+    echo "   ..."
+fi
+    
---
+++ slice_video.sh
@@ -0,0 +1,1 @@
+ffmpeg -i $1 -ss $2 -t $3 -vcodec copy -acodec copy $4
